ec4e2c74db915ae1649dfc58d5b99481534c144a
Matt <matt@CMDVMOSXLT154.local>
2016-03-19 16:15:46 -0400

    Changed my mind. Now it's a two-switch system that doesn't need a flood-fill algo to work.

--- Lightswitch/build/source/Lightswitch.java
+++ Lightswitch/build/source/Lightswitch.java
@@ -14,179 +14,223 @@ import java.io.IOException;
 
 public class Lightswitch extends PApplet {
 
-WireManager wire_manager;
-Battery battery;
+DebugTool debugger;
+Lightbulb lightbulb;
+Generator generator;
+SwitchBox switchbox;
+
 public void setup() {
   
-  wire_manager = new WireManager();
-  battery = new Battery(300,300);
-  rectMode(CENTER);
+  rectMode(RADIUS);
+
+  debugger = new DebugTool(5);
+  lightbulb = new Lightbulb();
+  generator = new Generator();
+  switchbox = new SwitchBox();
 }
 
 public void draw() {
-  background(200);
-  wire_manager.draw();
-  battery.draw();
+  background (80);
+  lightbulb.draw();
+  switchbox.draw();
+  generator.draw();
+  //debugger.positionDebug();
 }
 
 public void mouseClicked() {
-  wire_manager.handleClick();
-}
+  //debugger.printMouseCoordinates();
 
-public void keyPressed()
-{
-  if (key == 'r'){
-    wire_manager.resetWires();
+  if (switchbox.collider.isWithin(mouseX,mouseY)) {
+    switchbox.toggleSwitchbox();
+  }
+
+  if (generator.boxCollider.isWithin(mouseX,mouseY)) {
+    generator.turnGenerator();
   }
 }
 
-class Conductor {
+class CollisionBox {
+  int x;
+  int y;
+  int xRad;
+  int yRad;
+  public CollisionBox(int x, int y, int xRadius, int yRadius) {
+    this.x = x;
+    this.y = y;
+    xRad = xRadius;
+    yRad = yRadius;
+  }
 
-    boolean live;
+  public boolean withinX (int checkX) {
+    return checkX <= (x + xRad/2.0f) && checkX >= (x - xRad/2.0f);
+  }
 
-    public Conductor()
+  public boolean withinY (int checkY) {
+    return checkY <= (y + yRad/2.0f) && checkY >= (y - yRad/2.0f);
+  }
+
+  public boolean isWithin(int checkX, int checkY) {
+    return withinX(checkX) && withinY(checkY);
+  }
+}
+
+class DebugTool {
+
+  int pX;
+  int pY;
+  int sens;
+
+  public DebugTool(int sensitivity) {
+    pX = 0;
+    pY = 0;
+    sens = sensitivity;
+  }
+
+  public void positionDebug() {
+    line(mouseX,0,mouseX,height);
+    line(0,mouseY,width,mouseY);
+    if ( pow(pow(abs(mouseX - pX),2)+pow(abs(mouseY - pY),2),0.5f) > sens)
     {
-
+      printMouseCoordinates();
     }
-
-    public void toggle_active() {
-
-    }
-
-    public void update() {
-      if (live)
-      {
-        fill(255,255,0);
-      }
-      else {
-        fill(100);
-      }
-    }
-
-    public void activated() {
-
-    }
-}
-
-class Battery {
-
-  private int centerX;
-  private int centerY;
-  private BatteryPole pole;
-  public Battery(int x, int y)
-  {
-    centerX = x;
-    centerY = y;
-    pole = new BatteryPole(x,y-20,30,10);
+    pX = mouseX;
+    pY = mouseY;
   }
 
-  public void draw()
-  {
-    int fill = g.fillColor;
+  public void printMouseCoordinates() {
+    println("(" + mouseX + "," + mouseY + ")");
+  }
+}
+
+class Lightbulb {
+
+  int maxBrightness;
+  int currentBrightness;
+  float brightnessDegrade;
+
+  public Lightbulb() {
+    maxBrightness = 240;
+    currentBrightness = 0;
+    brightnessDegrade = 3;
+  }
+
+  public void draw() {
+    if (switchbox.on && frameCount % 3 == 0) {
+      currentBrightness = maxBrightness;
+  }
+    strokeWeight(1);
+    fill(240,240,0,currentBrightness);
+    ellipse(250,100,50,90);
+    fill(187);
+    rect(250,110,30,4,10);
+    rect(250,118,30,4,10);
+    rect(250,126,25,4,10);
+    rect(250,134,20,4,10);
+    rect(250,142,15,4,10);
     fill(0);
-    rect(centerX, centerY, 30,50);
-    rect(centerX, centerY-10, 10, 50);
-    pole.draw();
-    fill(fill);
+    rect(250,150,5,4,10);
+    line(256,106,261,83);
+    line(244,106,239,83);
+    strokeWeight(2);
+    line(250,150,250,250);
+    line(400,250,100,250);
+    line(100,250,100,350);
+    for (int r = currentBrightness; r > 50; r-=5) {
+      noStroke();
+      fill(250,250,0,5);
+      ellipse(250,94,r,r);
+      stroke(0);
+    }
+    if (currentBrightness > 0) {
+      currentBrightness -= brightnessDegrade;
+    }
+  }
+
+  public void addBrightness(int lumens) {
+    if (currentBrightness < 255) {
+      currentBrightness += lumens;
+    }
   }
 }
 
-class BatteryPole extends Conductor {
+class Generator {
+  int clickedFill;
+  float spinSpeed;
+  float angleGoTo;
+  float originAngle;
+  float currentAngle;
+  float t;
+  CollisionBox boxCollider;
 
-  private int centerX;
-  private int centerY;
-  private int radiusX;
-  private int radiusY;
-  public BatteryPole(int x, int y, int radX, int radY) {
-    centerX = x;
-    centerY = y;
-    radiusX = radX;
-    radiusY = radY;
-    live = true;
+  public Generator() {
+    boxCollider = new CollisionBox(300,400,25,25);
+    currentAngle = 0;
+    clickedFill = color(120);
   }
-   public void draw() {
-     int s = g.strokeColor;
-     noStroke();
-     update();
-     rect(centerX, centerY, radiusX, radiusY);
-     stroke(s);
+
+  public void draw() {
+    strokeWeight(2);
+    line(400,250,400,350);
+    line(300,400,350,400);
+    rect(300,400,25,25,10);
+    translate(400,400);
+    currentAngle = lerp(originAngle,angleGoTo,t);
+    rotate(currentAngle);
+    ellipse(0,0,100,100);
+    line(-50,0,50,0);
+    line(0,-50,0,50);
+    rotate(40);
+    line(-50,0,50,0);
+    line(0,-50,0,50);
+    translate(-400,-400);
+    if (t < 1) {
+      t += (2/frameRate)*(1-t);
+    }
+  }
+
+  public void turnGenerator() {
+    t = 0;
+    originAngle = currentAngle;
+    lightbulb.addBrightness(30);
+    angleGoTo += 1;
   }
 }
 
-class WireManager {
+class SwitchBox {
+  int onPosition;
+  int offPosition;
+  int position;
+  boolean on;
+  CollisionBox collider;
 
-  Wire w;
-  private ArrayList<Wire> wires;
-  boolean drawing;
-
-  public WireManager()
-  {
-    drawing = false;
-    wires = new ArrayList<Wire>();
+  public SwitchBox() {
+    on = false;
+    onPosition = 378;
+    offPosition = 423;
+    position = offPosition;
+    collider = new CollisionBox(100,400,20,46);
   }
 
-  public void handleClick()
-  {
-    if (drawing){
-      stopDrawing();
+  public void toggleSwitchbox() {
+    if (on) {
+      position = offPosition;
     }
     else {
-      startDrawing();
+      position = onPosition;
     }
-    drawing = !drawing;
+    on = !on;
   }
 
-  private void startDrawing()
-  {
-    w = new Wire(mouseX,mouseY);
+  public void draw() {
+    fill(190);
+    rect(100,400,25,50);
+    fill(20);             // Off fill
+    rect(100,377,20,23);  // Off background
+    fill(230,230,0);      // On fill
+    rect(100,423,20,23);  // On background
+    fill(230);
+    rect(100,position,20,23,5);
   }
-
-  private void stopDrawing()
-  {
-    w.createWire(mouseX,mouseY);
-    wires.add(w);
-  }
-
-  public void draw()
-  {
-    if (wires.size() > 0) {
-      for (Wire w : wires)
-      {
-        w.draw();
-      }
-    }
-  }
-
-  public void resetWires(){
-    wires = new ArrayList<Wire>();
-    w = null;
-    drawing = false;
-  }
-}
-
-class Wire {
-
-  private int startingX;
-  private int startingY;
-  private int endingX;
-  private int endingY;
-  public Wire(int x,
-              int y) {
-    startingX = x;
-    startingY = y;
-  }
-
-  public void createWire(int x, int y) {
-      println("Drawing a line at " + startingX + ", " + startingY + " to " + x + ", " + y);
-      endingX = x;
-      endingY = y;
-  }
-
-  public void draw()
-  {
-    line(startingX,startingY,endingX,endingY);
-  }
-
 }
   public void settings() {  size(500,500); }
   static public void main(String[] passedArgs) {
